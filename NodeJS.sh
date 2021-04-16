25729823cbda68393cdad33bc92762a9559f0571
iqrok <blogo3x@gmail.com>
2021-04-17 02:28:53 +0700

    change pm2 app start with json file

--- NodeJS.sh
+++ NodeJS.sh
@@ -9,6 +9,6 @@ docroot=$5
 mkdir "$home/$user/web/$domain/nodeapp"
 chown -R $user:$user "$home/$user/web/$domain/nodeapp"
 rm "$home/$user/web/$domain/nodeapp/app.sock"
-runuser -l $user -c "pm2 start $home/$user/web/$domain/nodeapp/app.js"
+runuser -l $user -c "pm2 start $home/$user/web/$domain/pm2.json"
 sleep 5
 chmod 777 "$home/$user/web/$domain/nodeapp/app.sock"
